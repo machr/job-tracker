695e8763576986f204b25a9e9cfb3a1139ac2c50
Mark Powell <hi@powellmark.com>
2017-04-05 08:58:57 +1000

    added handlebars template for job listings and connected api

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,14 +3,15 @@ console.log('log');
 function listingsIndex(){
   var apiUrl = '/api/job_listings/';
   var jobListingTemplate = Handlebars.compile( $('#job-listing-template').html() );
+  var editListingTemplate = Handlebars.compile( $('#edit-listing-template').html() );
   $.ajax({ url: apiUrl }).done(renderIndex);
 
-  // $.ajax({
-  //   url: apiUrl,
-  //   method: 'get'
-  // }).done(function(listings){
-  //   console.log(listings);
-  // });
+  function renderEditForm(event){
+    var listingId = $(event.target).closest('.job-listing').data('id');
+    console.log(listingId);
+  }
+
+  $('.job-listings').on('click', '.job-listing', renderEditForm);
 
   function renderIndex(listings){
     console.log(listings);
@@ -20,6 +21,49 @@ function listingsIndex(){
     });
   }
 }
+
+// $('#edit-card').on('submit', function(event){
+// //       event.preventDefault();
+// //       console.log(cardId);
+// //       $.ajax({
+// //         url: '/api/cards/' + cardId,
+// //         data: {
+// //           name: $('.edit-card-name').val(),
+// //           image_url: $('.edit-card-image-url').val()
+// //         },
+// //         method: 'PUT'
+// //       }).done(function(card){
+// //         console.log(card);
+// //       });
+// //     });
+
+
+
+// $('.container').on('click', '.edit-btn', function(event){
+//     $('.modal-wrapper').addClass('show');
+//     var cardId = $(event.target).closest('.card').data('id');
+//
+//     $('.modal-wrapper').on('click', '.modal-close-btn', function(event){
+//       $('.modal-wrapper').removeClass('show');
+//     });
+//
+//
+//     $('#edit-card').on('submit', function(event){
+//       event.preventDefault();
+//       console.log(cardId);
+//       $.ajax({
+//         url: '/api/cards/' + cardId,
+//         data: {
+//           name: $('.edit-card-name').val(),
+//           image_url: $('.edit-card-image-url').val()
+//         },
+//         method: 'PUT'
+//       }).done(function(card){
+//         console.log(card);
+//       });
+//     });
+//   });
+
 
 
 
--- app/controllers/api/job_listings_controller.rb
+++ app/controllers/api/job_listings_controller.rb
@@ -1,6 +1,7 @@
 class Api::JobListingsController < ApplicationController
   def index
     @JobListings = User.all.find(session[:user_id]).job_listings
+    # This should be on the Dashboard page. INdes should display all listings
     render json: @JobListings.to_json
   end
 
