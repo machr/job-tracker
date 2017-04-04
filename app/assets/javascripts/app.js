ce7d0686759153608ee49fa32bcbbba64be01557
Mark Powell <hi@powellmark.com>
2017-04-04 16:14:16 +1000

    created login in topnav + show collection of listings on root

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,30 @@
 console.log('log');
+
+function listingsIndex(){
+  var apiUrl = '/api/job_listings/';
+  var jobListingTemplate = Handlebars.compile( $('#job-listing-template').html() );
+  $.ajax({ url: apiUrl }).done(renderIndex);
+
+  // $.ajax({
+  //   url: apiUrl,
+  //   method: 'get'
+  // }).done(function(listings){
+  //   console.log(listings);
+  // });
+
+  function renderIndex(listings){
+    console.log(listings);
+    listings.forEach(function(listing){
+      var html = jobListingTemplate(listing);
+      $('.job-listings').append(html);
+    });
+  }
+}
+
+
+
+
+$(document).ready(function() {
+  // Callback to Handlebar function
+  listingsIndex();
+});
