501e94bfe86bb606bf7445f3492a2f3d30c19098
full slack <helminems@hotmail.com>
2017-04-06 13:59:01 +1000

    modified registration validation password, add wireframes folder and edit CSS on registration.html page

--- README.md
+++ README.md
@@ -22,14 +22,13 @@
 
 ## General Approach
 
-
 First step that we did, was to brainstorm for the user story and did a rough sketch of what our website would look like. We then wrote down the database table and models of what is needed for our API so that we know what are the primary keys and relationships for our database. After coming up with the database, we designed the wireframes decided on what technologies we would use to run our API on.
 
 
 After we have all the components and ideas of how our website would look like, we then divide the task of building our website.
 
-http://user_stories
+User story ==> https://trello.com/b/el27sM6w/job-diary
 
-http://wireframes
+Wireframes can be found in the wireframes folder.
 
 ## Unsolved Problems or Major Hurdles:
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,15 +3,20 @@ class SessionController < ApplicationController
   end
 
   def register
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-
-      session[:user_id] = user.id
-      redirect_to '/'
+    user = User.new
+    user.name = params[:name]
+    user.email = params[:email]
+    user.password = params[:password]
+    if user.save
+      @message = "Registration Successful"
+      render :message
     else
-      render :register
+      @failed = true
+      @message = "Sorry Please Register Again"
+      render :message
     end
   end
+
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
@@ -22,8 +27,13 @@ class SessionController < ApplicationController
       redirect_to '/'
     end
   end
+
   def destroy
     session[:user_id] = nil
     redirect_to '/'
   end
+
+  # def message
+  #
+  # end
 end
