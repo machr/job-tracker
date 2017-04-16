7eaeee363726474e5194d6e07655faa28d4b4cd3
Soul Wong <sou.wei.wong@gmail.com>
2017-04-16 15:03:25 +0800

    fixed monthly query in API job applications and added empty arguments handling

--- app/controllers/api/job_applications_controller.rb
+++ app/controllers/api/job_applications_controller.rb
@@ -4,19 +4,36 @@ class Api::JobApplicationsController < ApplicationController
     #get job applications by x num of weeks ago
     if params.include?(:w)
       if params.include?(:offset)
-        num = params[:offset].to_i
-        render json: current_user_weekly_job_applications(params[:w]).limit(10).offset(num)
+        #check if empty arguments supplied
+        if params[:w].present? && params[:offset].present?
+          num = params[:offset].to_i
+          render json: current_user_weekly_job_applications(params[:w]).limit(10).offset(num)
+        else
+          render json: { message: "error, invalid week / offset number"}
+        end
       else
-        render json: current_user_weekly_job_applications(params[:w]).limit(10)
+        if params[:w].present?
+          render json: current_user_weekly_job_applications(params[:w]).limit(10)
+        else
+          render json: { message: "error, invalid week number"}
+        end
       end
 
     #get job applications by x num of months ago
     elsif params.include?(:m)
       if params.include?(:offset)
-        num = params[:offset].to_i
-        render json: current_user_monthly_job_applications(params[:w]).limit(10).offset(num)
+        if params[:m].present? && params[:offset].present?
+          num = params[:offset].to_i
+          render json: current_user_monthly_job_applications(params[:m]).limit(10).offset(num)
+        else
+          render json: { message: "error, invalid month / offset number"}
+        end
       else
-        render json: current_user_monthly_job_applications(params[:w]).limit(10)
+        if params[:m].present?
+          render json: current_user_monthly_job_applications(params[:m]).limit(10)
+        else
+          render json: { message: "error, invalid month number"}
+        end
       end
 
     #default gets all job applications if no weeks or month is specified in params
